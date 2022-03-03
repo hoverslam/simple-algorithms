be6ce188677aec26373f55ce5c0dc46870b7996b
Christopher Hofmann <hofmann.christopher@outlook.com>
2022-03-03 14:20:06 +0100

    Add bubble sort algorithm

---
+++ main/bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(unsorted_list):
+    my_list = unsorted_list[:]   
+    while True:
+        swapped = False
+        for i in range(1, len(my_list)):
+            if my_list[i-1] > my_list[i]:
+                my_list[i-1], my_list[i] = my_list[i], my_list[i-1]
+                swapped = True
+                
+        if not swapped:
+            return my_list
+    
+unsorted_list = [5, 3, 9, 2, 5, 1, 4, 2, 8]   
+sorted_list = bubble_sort(unsorted_list)
+print(unsorted_list)
+print(sorted_list)
